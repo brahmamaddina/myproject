1f3f44f3741c7be1a370c3e05dc8f8906dadfcd1
brahmamaddina <brahmatejamaddina@gmail.com>
2024-06-26 19:14:06 +0530

    page js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit {
   title = 'teja maddina';
-  userName = ''
-  password = ''
+  userName = '';
+  password = '';
+  name = '';
+  number = '';
+  city = '';
+  textarea = '';
+  formdata: any = '';
+  isSubmitted : boolean = false;
 
   ngOnInit() {
     console.log("test hello world teja" );
+
+    this.formdata = new FormGroup({
+      name: new FormControl("Brahma teja"),
+      number: new FormControl(8008229977),
+      city: new FormControl("option"),
+      address: new FormControl("Robert Robertson, 1234 NW Bobcat Lane, St. Robert, MO 65584-5678"),
+      gender: new FormControl("male"),
+   });
   }
   ngOnChanges() {
     console.log("data:", this.userName, this.password);
@@ -24,6 +39,11 @@ export class AppComponent implements OnInit, OnChanges {
   public login() : void {
     console.log("login values:", this.userName, this.password)
     
+  }
+
+  public mySubmit(value?: any) : void {
+    console.log("submit values:", value)
+    this.isSubmitted = true;
   }
 
 }
